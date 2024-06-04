7e2459f6990cfb2eecaed2aae8266157c8ae19c1
yahya akli <akli.yahya05@gmail.com>
2024-06-04 12:25:33 +0100

    yahya

--- resources/views/product/view.blade.php
+++ resources/views/product/view.blade.php
@@ -1,54 +1,16 @@
 <x-app-layout>
-    {{-- <div class="container mx-auto px-4">
+    <div class="container mx-auto px-4">
         <div class="row gap-4 flex flex-col lg:flex-row">
             <div class="col text-center flex-1">
                 <img src="{{ $product->image }}" alt="{{ $product->title }}" class="max-w-full h-auto">
             </div>
-            <div class="lg:col-span-2">
-                <h1 class="text-lg font-semibold">
-                    {{ $product->title }}
-                </h1>
-                <div class="text-xl font-bold mb-6">${{ $product->price }}</div>
-                <div class="flex items-center justify-between mb-5">
-                    <label for="quantity" class="block font-bold mr-4">
-                        Quantity
-                    </label>
-                    <input type="number" name="quantity" x-ref="quantityEl" value="1" min="1"
-                        class="w-32 focus:border-purple-500 focus:outline-none rounded" />
-                </div>
-                <button @click="addToCart($refs.quantityEl.value)"
-                    class="btn-primary py-4 text-lg flex justify-center min-w-0 w-full mb-6">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                    </svg>
-                    Add to Cart
-                </button>
-                <div class="mb-6" x-data="{ expanded: false }">
-                    <div x-show="expanded" x-collapse.min.120px class="text-gray-500 wysiwyg-content">
-                        {{ $product->description }}
-                    </div>
-                    <p class="text-right">
-                        <a @click="expanded = !expanded" href="javascript:void(0)"
-                            class="text-purple-500 hover:text-purple-700"
-                            x-text="expanded ? 'Read Less' : 'Read More'"></a>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>  --}}
-    <div class="container">
-        <div class="row gap-4">
-            <div class="col text-center">
-                <img src="{{ $product->image }}" alt="{{ $product->title }}">
-            </div>
-            <div style="overflow-wrap: break-word;" class="col gap-3 d-flex flex-column fs-6 text-gray-600">
+            <div class="col gap-3 d-flex flex-column fs-6 text-gray-600 flex-1">
                 <div class="ProdTitle text-black">
                     <h1 class="font-bold fs-4">(Product 21) Sample - Computers & Accessories For Sale</h1>
                 </div>
                 <div class="descSection gap-2 d-flex flex-column">
-                    <div class="description">{{ $product->description }}</div>
+                    <div class="description">Nam tempus turpis at metus scelerisque placerat nulla deumantos solicitud
+                        felis. Pellentesque diam dolor, elementum etos lobortis...</div>
                     <div>Vendor: Ella - Halothemes<br>
                         SKU: KJSU-58636
                         <div class="Availability">Availability: In Stock</div>
